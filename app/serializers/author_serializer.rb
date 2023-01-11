304312ef4f79802f9bf1ef06949678f5976e1149
Rachel <rstark.smith@gmail.com>
2023-01-10 19:11:32 -0600

    commit tree 53f0886fbdb408ac2260bb3d53dba63a8aeb98d2

---
+++ app/serializers/author_serializer.rb
@@ -0,0 +1,8 @@
+class AuthorSerializer < ActiveModel::Serializer
+  attributes :name
+
+  has_one :profile, serializer: AuthorProfileSerializer
+  has_many :posts, serializer: AuthorPostSerializer
+
+ 
+end
